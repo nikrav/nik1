9e87b8b40d00454fa8b178ebd3ec6be1159f06d7
nikrav <130073710+nikrav@users.noreply.github.com>
2024-05-01 23:24:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({ 
     user: 'root',
     host: 'localhost',
-    password: 'password',
+    password: 'Vkvrrn123',
     database: 'employeesys',
 });
 
